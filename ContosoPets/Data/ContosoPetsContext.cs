badeb5e4d4247bdd5543a26202e2310d8e15f175
John Grant <john.grant94@hotmail.com>
2020-10-18 23:17:58 -0400

    initial commit

---
+++ ContosoPets/Data/ContosoPetsContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ContosoPets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPets.Data
+{
+    public class ContosoPetsContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ContosoConnectionString");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("Invalid connection string");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ ContosoPets/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using ContosoPets.Data;
+
+namespace ContosoPets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new ContosoPetsContext();
+
+            /*
+            var squeakyBone = new Product
+            {
+                Name = "Squeaky Dog Bone",
+                Price = 4.99M
+            };
+            context.Products.Add(squeakyBone);
+            
+            var tennisBalls = new Product
+            {
+                Name = "Tennis Ball 3-Pack",
+                Price = 9.99M
+            };
+            context.Add(tennisBalls);
+
+            context.SaveChanges();
+            */
+            
+            var products = context.Products
+                .Where(p => p.Price >= 5.00M)
+                .OrderBy(p => p.Name);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Id:\t{product.Id}");
+                Console.WriteLine($"Name:\t{product.Name}");
+                Console.WriteLine($"Price:\t{product.Price:C2}");
+                Console.WriteLine(new string('-', 20));
+            }
+        }
+    }
+}
